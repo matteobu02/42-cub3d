5a2122d74d1417af15693a7b6ac271107b5ad502
Matteo Bucci <mbucci@fu-r5-p5.s19.be>
2022-05-12 17:49:25 +0200

    basic checks done

--- main/main.c
+++ main/main.c
@@ -6,7 +6,7 @@
 /*   By: mbucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 18:31:24 by mbucci            #+#    #+#             */
-/*   Updated: 2022/05/12 14:27:28 by mbucci           ###   ########.fr       */
+/*   Updated: 2022/05/12 17:36:56 by mbucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,13 @@ void	close_error(char *msg)
 
 int	main(int ac, char **av)
 {
+	// Check number of arguments
 	if (ac != 2)
 		close_error("Error\nNeed exactly one '.cub' parameter");
+	// Check file name and accessibility
 	int fd = check_arg(av[1]);
+	// Check infos and map
+	// Build main struct
 	(void)fd;
 	return (0);
 }
--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: mbucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 13:48:48 by mbucci            #+#    #+#             */
-/*   Updated: 2022/05/12 14:58:37 by mbucci           ###   ########.fr       */
+/*   Updated: 2022/05/12 17:48:13 by mbucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,16 @@ int	check_arg(char *path)
 	int	fd;
 
 	if ((ft_strlen(path) < 5) || !ft_strrstr(path, ".cub"))
-		close_error("Error\nargument is not a '. cub' file");
+		close_error("Error\nArgument is not a '. cub' file");
 	fd = open(path, O_RDONLY);
 	if (fd < 1 || read(fd, NULL, 0) < 0)
-		close_error("Error\nfile can't be opened");
+		close_error("Error\nFile can't be opened");
 	return (fd);
 }
+
+/*void	check_file(int fd)
+{
+	char	**raw_map;
+
+	raw_map = (char **)malloc(sizeof(char *) *  )
+}*/
