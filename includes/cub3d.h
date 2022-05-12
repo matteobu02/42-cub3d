3b94b1d8a5e8be16dd3bf78e42b9cff7c28d1802
Matteo Bucci <mbucci@fu-r5-p5.s19.be>
2022-05-12 15:48:30 +0200

    mlx.h header added

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: mbucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 13:27:24 by mbucci            #+#    #+#             */
-/*   Updated: 2022/05/12 14:27:46 by mbucci           ###   ########.fr       */
+/*   Updated: 2022/05/12 15:47:59 by mbucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define CUB3D_H
 
 #include "libft.h"
+#include <mlx.h>
 #include <fcntl.h>
 
 typedef struct s_map
